ff27b2566bd1d11c3d950b726294478de5603370
João Victor Martins <48113822+jaovic@users.noreply.github.com>
2022-07-26 14:06:06 -0300

    ♻️

--- teste.ts
+++ teste.ts
@@ -1,18 +1,18 @@
-// const puppeteer = require("puppeteer");
-// async function BbBot (){
-// const browser = await puppeteer.launch({headless: false, executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe'});
-//   // cria uma nova pagina
-//   const page = await browser.newPage();
-//   // entra na url
-//   await page.goto("https://google.com");
-//   // aguarda aparecer a caixa de login
-//   console.log('dps do google');
-//   await page.type("[type=text]", "teste");
-//   await page.screenshot({ path: `${'teste'}.png` });
-//   await page.click(".gNO89b");
-//   await page.waitForTimeout(2000);
-//   await page.screenshot({ path: `${'teste2'}.png` });
+const puppeteer = require("puppeteer");
+async function BbBot (){
+const browser = await puppeteer.launch({headless: false, executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe'});
+  // cria uma nova pagina
+  const page = await browser.newPage();
+  // entra na url
+  await page.goto("https://google.com");
+  // aguarda aparecer a caixa de login
+  console.log('dps do google');
+  await page.type("[type=text]", "teste");
+  await page.screenshot({ path: `${'teste'}.png` });
+  await page.click(".gNO89b");
+  await page.waitForTimeout(2000);
+  await page.screenshot({ path: `${'teste2'}.png` });
 
-//   await browser.close();
-// }
-// BbBot();
+  await browser.close();
+}
+BbBot();
